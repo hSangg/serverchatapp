1d9a4e5ea8a1f514919658064faae66ad699027c
Hoai Sang <hoai.sang050@gmail.com>
2022-03-03 10:40:37 +0700

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const path = require("path")
 const cors = require("cors")
+
+//@port
 const port = process.env.PORT || 5000
 
 const authRoute = require("./routes/auth")
@@ -19,14 +21,7 @@ app.use("/images", express.static(path.join(__dirname, "public/images")))
 
 //middleware
 app.use(express.json())
-// app.use(cors())
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-  res.header("Access-Control-Allow-Headers", "Content-Type")
-  next()
-})
+app.use(cors())
 
 // @socket io
 
