15caab7eae6640a112392d1e4fefe5dd885398bd
Hoai Sang <hoai.sang050@gmail.com>
2022-03-03 07:28:22 +0700

    fist

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ app.use("/images", express.static(path.join(__dirname, "public/images")))
 
 //middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
-app.all("/", function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
   next()
 })
 
