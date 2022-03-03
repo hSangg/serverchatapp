ab0e03089954b08ca4a1b980a0e4be6f0ee8a35e
Hoai Sang <hoai.sang050@gmail.com>
2022-03-03 07:15:51 +0700

    hop cope commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-const { default: helmet } = require("helmet")
-const morgan = require("morgan")
-const multer = require("multer")
 const path = require("path")
 const cors = require("cors")
 const port = process.env.PORT || 5000
@@ -23,8 +20,12 @@ app.use("/images", express.static(path.join(__dirname, "public/images")))
 //middleware
 app.use(express.json())
 app.use(cors())
-app.use(helmet())
-app.use(morgan("common"))
+
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  next()
+})
 
 // @socket io
 
@@ -39,12 +40,6 @@ mongoose.connect(
     console.log("MONGOOSE CONNECTED")
   }
 )
-
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
-  next()
-})
 
 //routes
 app.use("/api/auth", authRoute)
